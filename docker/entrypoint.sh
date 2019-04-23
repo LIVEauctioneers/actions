0b3b06de9d147d7e25a0e2597e47a9613b95ae80
Adam Veldhousen <adam.veldhousen@liveauctioneers.com>
2019-04-23 00:11:25 -0500

    use raw-output to prevent json specific formatting

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,13 +4,13 @@
 export EVENT_JSON=$(cat ${GITHUB_EVENT_PATH})
 echo "Github Event JSON:\n${EVENT_JSON}"
 
-export PR_NUMBER=$(echo "$EVENT_JSON" | jq '.pull_request.number')
-export PR_STATE=$(echo "$EVENT_JSON" | jq '.pull_request.state')
-export PR_URL=$(echo "$EVENT_JSON" | jq '.pull_request.url')
-export PR_TITLE=$(echo "$EVENT_JSON" | jq '.pull_request.title')
-export PR_BODY=$(echo "$EVENT_JSON" | jq '.pull_request.body')
-export PR_CREATED=$(echo "$EVENT_JSON" | jq '.pull_request.created_at')
-export PR_MERGED=$(echo "$EVENT_JSON" | jq '.pull_request.merged')
-export PR_COMMITS=$(echo "$EVENT_JSON" | jq '.pull_request.commits')
+export PR_NUMBER=$(echo "$EVENT_JSON" | jq --raw-output '.pull_request.number')
+export PR_STATE=$(echo "$EVENT_JSON" | jq --raw-output '.pull_request.state')
+export PR_URL=$(echo "$EVENT_JSON" | jq --raw-output '.pull_request.url')
+export PR_TITLE=$(echo "$EVENT_JSON" | jq --raw-output '.pull_request.title')
+export PR_BODY=$(echo "$EVENT_JSON" | jq --raw-output '.pull_request.body')
+export PR_CREATED=$(echo "$EVENT_JSON" | jq --raw-output '.pull_request.created_at')
+export PR_MERGED=$(echo "$EVENT_JSON" | jq --raw-output '.pull_request.merged')
+export PR_COMMITS=$(echo "$EVENT_JSON" | jq --raw-output '.pull_request.commits')
 
 bash -c "docker $*"
